3204f389afcb4a1881cbff550a07e3506ed6a3a6
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-19 00:59:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -20,7 +20,8 @@ def meal_choice(protein, veg1, veg2)
    puts "A plate of #{protein} with #{veg1} and #{veg2}."
  end
  
- meal_choice("brocolli", "macaroni")
+ meal_choice(brocolli, macaroni)
+ 
 
  
  
